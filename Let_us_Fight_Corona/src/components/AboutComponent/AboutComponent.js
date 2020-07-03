8e71f2beb56125afba3bd655ce4b00b71f3ce04e
Meera-W <54815182+Meera-W@users.noreply.github.com>
2020-07-03 14:40:27 +0530

    added an 'email us' link

--- Let_us_Fight_Corona/src/components/AboutComponent/AboutComponent.js
+++ Let_us_Fight_Corona/src/components/AboutComponent/AboutComponent.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import MailIcon from '@material-ui/icons/Mail';
+import { Link } from 'react-router-dom';
 function About () 
 {
+    
     return (
         <div className="container-fluid">
 
@@ -12,9 +14,8 @@ function About ()
 
                 <div className="team_info " >
                     <h4>ABOUT US</h4><hr />
-                    <h5>We are a team of budding student web developers who took upon the idea of
-                    creating a well-versed dashboard regarding Corona Virus
-                    (the pandemic which we all are fighting worldwide) into a project and shaped it to bloom this website.</h5>
+                    <h5>We are a team of budding student web developers who worked on the idea of
+                    creating a well-versed dashboard regarding Corona Virus - the pandemic which we are fighting worldwide - into a project, which resulted in the creation of this website.</h5>
                     <br />
                     <h4>OUR TEAM</h4><hr />
                     <div className="Team_member_names">
@@ -24,10 +25,10 @@ function About ()
                                     Gauri Shewale  (CSE , VJTI 2019-23)
                         </li>
                                 <li>
-                                    Piyusha Bharambe (IT ,VJTI 2019-23)
+                                    Piyusha Bharambe (IT , VJTI 2019-23)
                         </li>
                                 <li>
-                                    Vaishnavi Bhadrashette (CSE ,VJTI 2019-23)
+                                    Vaishnavi Bhadrashette (CSE , VJTI 2019-23)
                         </li>
                                 <li>
                                     Meera Wadher (CSE , VJTI 2019-23)
@@ -39,7 +40,10 @@ function About ()
 
                     <h4>CONTACT US</h4>
                     <hr />
-                    <h5> <MailIcon /> teamincognito04@gmail.com</h5>
+                   {/* <h5> <MailIcon /> teamincognito04@gmail.com</h5> */}
+                   {/* <a href="mailto:teamincognito04@gmail.com?Subject=Hello%20world!">Michael</a> */}
+                   <h5> <MailIcon /><a href={`mailto:teamincognito04@gmail.com?Subject=Message%20for%20the%20team`} style={{color: '#A21675'}}> Email Us</a></h5>
+
 
 
                 </div>
