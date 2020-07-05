c8144a321e33ba8b07a74ece2100e1a389fa4470
gaurishewale20 <gaurishewale201201@gmail.com>
2020-07-05 13:28:24 +0530

    Row labels error for StateTable in Dashboard corrected

--- Let_us_Fight_Corona/src/components/Dashboard/StateTableRow.js
+++ Let_us_Fight_Corona/src/components/Dashboard/StateTableRow.js
@@ -3,14 +3,15 @@ import React from "react";
 import styles from "./StateTable.module.css";
 
 
-function StateTableRow ( { statename, recovered, confirmed, deaths } )
+function StateTableRow ( { statename, confirmed, recovered, deaths } )
 {
     return (
         <>
             <tr className={ styles.statetablerow }>
                 <td>{ statename }</td>
-                <td>{ recovered }</td>
+
                 <td>{ confirmed }</td>
+                <td>{ recovered }</td>
                 <td>{ deaths }</td>
 
             </tr>
